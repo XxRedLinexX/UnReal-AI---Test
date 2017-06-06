c0107637aafd6746d10d75e436dc3f24946c7a3c
XxRedLinexX <tmanning@cbglades.com>
2017-06-05 22:52:16 -0400

    Update data_logger.py

--- data_logger.py
+++ data_logger.py
@@ -120,7 +120,7 @@ def main(file_name, starting_value):
 
                     
         keys = key_check()
-        if 'J' in keys:
+        if 'K' in keys:
             if paused:
                 paused = False
                 print('unpaused!')
